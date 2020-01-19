408c3658d621e0a50cd309b72994881eb3a83261
Raúl Angel Jiménez <Raulangel51@users.noreply.github.com>
2020-01-19 10:31:44 -0600

    Prueba Metodo CambioEstacion
    Se creo el JUnit test parar probar el cambio de estacion

---
+++ PruebaCambioEstacion.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PruebaCambioEstacion {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		Controlador test = new Controlador();
+		test.cambioEstacion(true, true);
+		double resultado = test.getEstacion();
+		assertEquals(88.3, resultado);
+	}
+
+}
